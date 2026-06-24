708a80de650ea6baf0caa73099e573843af917a1
kundai3 <kundaingwereva3@gmail.com>
2026-06-24 13:29:21 +0200

    Add header structure with navigation links

--- header.php
+++ header.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AutoSpare Hub</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
 
+<header>
+    <div class="logo">
+        <h2>AutoSpare Hub</h2>
+    </div>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="products.php">Products</a>
+        <a href="request-part.php">Request Part</a>
+        <a href="contact.php">Contact</a>
+        <a href="cart.php">Cart</a>
+        <a href="login.php">Login</a>
+    </nav>
+</header>
