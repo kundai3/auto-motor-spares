f4fbffc78f2c47352a7a628bc30af968ee89ee62
kundai3 <kundaingwereva3@gmail.com>
2026-06-24 13:28:16 +0200

    Add functions to retrieve products and categories

--- functions.php
+++ functions.php
@@ -1,1 +1,21 @@
+<?php
+require 'config.php';
 
+function getProducts() {
+    global $conn;
+    $query = "SELECT * FROM products";
+    return mysqli_query($conn, $query);
+}
+
+function getProductById($id) {
+    global $conn;
+    $query = "SELECT * FROM products WHERE id='$id'";
+    return mysqli_query($conn, $query);
+}
+
+function getCategories() {
+    global $conn;
+    $query = "SELECT * FROM categories";
+    return mysqli_query($conn, $query);
+}
+?>
